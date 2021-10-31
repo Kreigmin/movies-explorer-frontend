0855aac5bfda242286b090b837f4d6d10f69b867
Dmitriy Sobolev <leejjuk@gmail.com>
2021-10-31 17:34:52 +0200

    feat: create MoviesCard component and styles for it

---
+++ src/components/MoviesCard/MoviesCard.js
@@ -0,0 +1,44 @@
+import React, { useState } from "react";
+import { Switch, Route } from "react-router-dom";
+
+function MoviesCard({ id, card }) {
+  const [isLiked, setIsLiked] = useState(false);
+
+  const cardBookmarkClassName = `movies-card__bookmark ${
+    isLiked
+      ? "movies-card__bookmark_state_active"
+      : "movies-card__bookmark_state_inactive"
+  }`;
+
+  function handleCardLike() {
+    setIsLiked(!isLiked);
+  }
+
+  return (
+    <li className="movies-card">
+      <div className="movies-card__top">
+        <div className="movies-card__description">
+          <h2 className="movies-card__title">{card.nameRU}</h2>
+          <p className="movies-card__duration">{card.duration}</p>
+        </div>
+        <Switch>
+          <Route exact path="/movies">
+            <button
+              className={cardBookmarkClassName}
+              type="button"
+              onClick={handleCardLike}
+            ></button>
+          </Route>
+          <Route path="/saved-movies">
+            <button className="movies-card__delete-btn" type="button">
+              &times;
+            </button>
+          </Route>
+        </Switch>
+      </div>
+      <img className="movies-card__img" src={card.image} alt={card.nameRU} />
+    </li>
+  );
+}
+
+export default MoviesCard;

b6fbaa597350a65791d5d32e1ab65722c7129b4d
Dmitriy Sobolev <leejjuk@gmail.com>
2021-11-14 16:03:48 +0200

    feat: add logic for display movie cards for movies and saved movies routes

--- src/components/MoviesCardList/MoviesCardList.js
+++ src/components/MoviesCardList/MoviesCardList.js
@@ -1,23 +1,75 @@
 import React from "react";
 import MoviesCard from "../MoviesCard/MoviesCard";
-import { Route } from "react-router-dom";
+import { Route, Switch } from "react-router-dom";
 import Preloader from "../Preloader/Preloader";
 
-function MoviesCardList({ cards }) {
+function MoviesCardList({
+  movies,
+  visible,
+  loadMore,
+  isSubmitting,
+  onDeleteMovie,
+  onHandleBookmark,
+  savedMovies,
+  isThereSortetMovies,
+  isThereSortedSavedMovies,
+}) {
   return (
     <section className="cards">
       <div className="container">
-        <Preloader />
-        <ul className="cards__list">
-          {cards.map((card) => (
-            <MoviesCard key={card.id} card={card} />
-          ))}
-        </ul>
-        <Route exact path="/movies">
-          <button className="cards__more-btn" type="button">
-            Ещё
-          </button>
-        </Route>
+        <Switch>
+          <Route exact path="/movies">
+            {isSubmitting ? (
+              <Preloader />
+            ) : movies.length !== 0 ? (
+              <ul className="cards__list">
+                {movies.slice(0, visible).map((movie) => (
+                  <MoviesCard
+                    key={movie.id}
+                    movie={movie}
+                    movies={movies}
+                    onHandleBookmark={onHandleBookmark}
+                    savedMovies={savedMovies}
+                  />
+                ))}
+              </ul>
+            ) : isThereSortetMovies ? (
+              <></>
+            ) : (
+              <p className="cards__nothing-found-text">Ничего не найдено</p>
+            )}
+            {visible < movies.length && (
+              <button
+                className="cards__more-btn"
+                type="button"
+                onClick={loadMore}
+              >
+                Ещё
+              </button>
+            )}
+          </Route>
+          <Route path="/saved-movies">
+            {isSubmitting ? (
+              <Preloader />
+            ) : movies.length !== 0 ? (
+              <ul className="cards__list">
+                {movies.map((movie) => (
+                  <MoviesCard
+                    key={movie.id}
+                    id={movie.id}
+                    movie={movie}
+                    onDeleteMovie={onDeleteMovie}
+                    savedMovies={savedMovies}
+                  />
+                ))}
+              </ul>
+            ) : isThereSortedSavedMovies ? (
+              <></>
+            ) : (
+              <p className="cards__nothing-found-text">Ничего не найдено</p>
+            )}
+          </Route>
+        </Switch>
       </div>
     </section>
   );

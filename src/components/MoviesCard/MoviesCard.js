4ed7c8cd7e6be3ab45d94868713aaffb93970de6
Dmitriy Sobolev <leejjuk@gmail.com>
2021-11-14 16:01:04 +0200

    feat: add trailer button, time convert function

--- src/components/MoviesCard/MoviesCard.js
+++ src/components/MoviesCard/MoviesCard.js
@@ -1,42 +1,95 @@
 import React, { useState } from "react";
 import { Switch, Route } from "react-router-dom";
 
-function MoviesCard({ id, card }) {
+function MoviesCard({ movie, onDeleteMovie, onHandleBookmark, savedMovies }) {
   const [isLiked, setIsLiked] = useState(false);
 
+  const isBookmark = savedMovies.some((i) => i.nameRU === movie.nameRU);
+
   const cardBookmarkClassName = `movies-card__bookmark ${
-    isLiked
+    isBookmark || isLiked
       ? "movies-card__bookmark_state_active"
       : "movies-card__bookmark_state_inactive"
   }`;
 
-  function handleCardLike() {
-    setIsLiked(!isLiked);
+  function timeConvert(time) {
+    const hours = Math.floor(time / 60);
+    const minutes = time % 60;
+    return hours + "ч " + minutes + "м";
+  }
+
+  function handleMovieBookmark() {
+    if (isBookmark) {
+      setIsLiked(false);
+    } else {
+      setIsLiked(!isLiked);
+    }
+    onHandleBookmark(movie);
+  }
+
+  function handleMovieDelete() {
+    onDeleteMovie(movie._id);
   }
 
   return (
     <li className="movies-card">
-      <div className="movies-card__top">
-        <div className="movies-card__description">
-          <h2 className="movies-card__title">{card.nameRU}</h2>
-          <p className="movies-card__duration">{card.duration}</p>
-        </div>
-        <Switch>
-          <Route exact path="/movies">
+      <Switch>
+        <Route exact path="/movies">
+          <div className="movies-card__top">
+            <div className="movies-card__description">
+              <h2 className="movies-card__title">{movie.nameRU}</h2>
+              <p className="movies-card__duration">
+                {timeConvert(movie.duration)}
+              </p>
+            </div>
+
             <button
               className={cardBookmarkClassName}
               type="button"
-              onClick={handleCardLike}
+              onClick={handleMovieBookmark}
             ></button>
-          </Route>
-          <Route path="/saved-movies">
-            <button className="movies-card__delete-btn" type="button">
+          </div>
+          <a
+            className="movies-card__trailer-link"
+            href={movie.trailerLink}
+            target="_blank"
+          >
+            <img
+              className="movies-card__img"
+              src={`https://api.nomoreparties.co${movie.image.url}`}
+              alt={movie.nameRU}
+            />
+          </a>
+        </Route>
+        <Route path="/saved-movies">
+          <div className="movies-card__top">
+            <div className="movies-card__description">
+              <h2 className="movies-card__title">{movie.nameRU}</h2>
+              <p className="movies-card__duration">
+                {timeConvert(movie.duration)}
+              </p>
+            </div>
+            <button
+              className="movies-card__delete-btn"
+              type="button"
+              onClick={handleMovieDelete}
+            >
               &times;
             </button>
-          </Route>
-        </Switch>
-      </div>
-      <img className="movies-card__img" src={card.image} alt={card.nameRU} />
+          </div>
+          <a
+            className="movies-card__trailer-link"
+            href={movie.trailerLink}
+            target="_blank"
+          >
+            <img
+              className="movies-card__img"
+              src={movie.image}
+              alt={movie.nameRU}
+            />
+          </a>
+        </Route>
+      </Switch>
     </li>
   );
 }

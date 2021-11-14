7d6308a14148a2e9d964b43d7d13e2f9d3dd143f
Dmitriy Sobolev <leejjuk@gmail.com>
2021-11-14 15:58:11 +0200

    feat: add all props for nested components

--- src/components/Movies/Movies.js
+++ src/components/Movies/Movies.js
@@ -1,99 +1,40 @@
 import React from "react";
 import MoviesCardList from "../MoviesCardList/MoviesCardList";
 import SearchForm from "../SearchForm/SearchForm";
-import womanAndChildren from "../../images/woman-and-children.jpg";
-import oldFormatPhoto from "../../images/old-format-photo.jpg";
-import manWithGuitar from "../../images/man-with-guitar.jpg";
-import hall from "../../images/hall.jpg";
-import peopleOnSkateboard from "../../images/people-on-skateboard.jpg";
-import roomWithBooks from "../../images/room-with-books.jpg";
-import men from "../../images/men.jpg";
-import graffiti from "../../images/graffiti.jpg";
-import marathon from "../../images/marathon.jpg";
-import party from "../../images/party.jpg";
-import manWithSigarette from "../../images/man-with-sigarette.jpg";
-import workingMan from "../../images/working-man.jpg";
 
-function Movies() {
-  const cards = [
-    {
-      id: 1,
-      nameRU: "33 слова о дизайне",
-      duration: "1ч 47м",
-      image: womanAndChildren,
-    },
-    {
-      id: 2,
-      nameRU: "33 слова о дизайне",
-      duration: "1ч 47м",
-      image: oldFormatPhoto,
-    },
-    {
-      id: 3,
-      nameRU: "33 слова о дизайне",
-      duration: "1ч 47м",
-      image: manWithGuitar,
-    },
-    {
-      id: 4,
-      nameRU: "33 слова о дизайне",
-      duration: "1ч 47м",
-      image: hall,
-    },
-    {
-      id: 5,
-      nameRU: "33 слова о дизайне",
-      duration: "1ч 47м",
-      image: peopleOnSkateboard,
-    },
-    {
-      id: 6,
-      nameRU: "33 слова о дизайне",
-      duration: "1ч 47м",
-      image: roomWithBooks,
-    },
-    {
-      id: 7,
-      nameRU: "33 слова о дизайне",
-      duration: "1ч 47м",
-      image: men,
-    },
-    {
-      id: 8,
-      nameRU: "33 слова о дизайне",
-      duration: "1ч 47м",
-      image: graffiti,
-    },
-    {
-      id: 9,
-      nameRU: "33 слова о дизайне",
-      duration: "1ч 47м",
-      image: marathon,
-    },
-    {
-      id: 10,
-      nameRU: "33 слова о дизайне",
-      duration: "1ч 47м",
-      image: party,
-    },
-    {
-      id: 11,
-      nameRU: "33 слова о дизайне",
-      duration: "1ч 47м",
-      image: manWithSigarette,
-    },
-    {
-      id: 12,
-      nameRU: "33 слова о дизайне",
-      duration: "1ч 47м",
-      image: workingMan,
-    },
-  ];
-
+function Movies({
+  onSearchFormSubmit,
+  movies,
+  visible,
+  loadMore,
+  isChecked,
+  onChecked,
+  onSearchText,
+  onRenderLoading,
+  isSubmitting,
+  onHandleBookmark,
+  savedMovies,
+  isThereSortetMovies,
+}) {
   return (
     <main className="main">
-      <SearchForm />
-      <MoviesCardList cards={cards} />
+      <SearchForm
+        onSearchFormSubmit={onSearchFormSubmit}
+        onChecked={onChecked}
+        onSearchText={onSearchText}
+        onRenderLoading={onRenderLoading}
+        isChecked={isChecked}
+      />
+      <MoviesCardList
+        movies={movies}
+        visible={visible}
+        loadMore={loadMore}
+        isSubmitting={isSubmitting}
+        onHandleBookmark={onHandleBookmark}
+        savedMovies={savedMovies}
+        sortedMovies={movies}
+        isThereSortetMovies={isThereSortetMovies}
+      />
     </main>
   );
 }

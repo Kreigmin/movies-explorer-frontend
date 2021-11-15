b88e82517cb566a0ebf9173f2be030844cc50733
Dmitriy Sobolev <leejjuk@gmail.com>
2021-11-15 17:42:54 +0200

    refactor: add Header

--- src/components/Profile/Profile.js
+++ src/components/Profile/Profile.js
@@ -1,6 +1,7 @@
 import React, { useContext, useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import { CurrentUserContext } from "../../contexts/CurrentUserContext";
+import Header from "../Header/Header";
 
 function Profile({
   onSignOut,
@@ -9,6 +10,7 @@ function Profile({
   errorInfoText,
   successStatus,
   setSuccessStatus,
+  loggedIn,
 }) {
   const [isClicked, setIsClicked] = useState(false);
   const [name, setName] = useState("");
@@ -63,104 +65,113 @@ function Profile({
   }
 
   return (
-    <section className="profile">
-      <div className="profile__container">
-        <h2 className="profile__title">{`Привет, ${currentUser.name}!`}</h2>
-        <form className="profile__form" name="profile" onSubmit={handleSubmit}>
-          <fieldset className="profile__form-field">
-            <div className="profile__input-container">
-              <span className="profile__form-input-name">Имя</span>
-              <input
-                className="profile__form-input"
-                type="text"
-                id="profile-name-input"
-                name="profileName"
-                value={name || ""}
-                minLength="2"
-                maxLength="30"
-                required={true}
-                noValidate
-                autoComplete="off"
-                disabled={isClicked ? false : errorStatus ? false : true}
-                onChange={handleNameChange}
-              />
-            </div>
-            <span
-              className={`form__input-error name-input-error ${
-                !nameValidation.isNameValid ? "form__input-error_active" : ""
-              }`}
-            >
-              {nameValidation.nameValidationMessage}
-            </span>
-          </fieldset>
-          <fieldset className="profile__form-field">
-            <div className="profile__input-container">
-              <span className="profile__form-input-name">E-mail</span>
-              <input
-                className="profile__form-input"
-                id="profile-email-input"
-                type="email"
-                name="profileEmail"
-                value={email || ""}
-                autoComplete="off"
-                required={true}
-                noValidate
-                disabled={isClicked ? false : errorStatus ? false : true}
-                onChange={handleEmailChange}
-              />
-            </div>
-            <span
-              className={`form__input-error profile-email-input-error ${
-                !emailValidation.isEmailValid ? "form__input-error_active" : ""
-              }`}
-            >
-              {emailValidation.emailValidationMessage}
-            </span>
-          </fieldset>
-          <div className="profile__footer">
-            {errorStatus ? (
-              <span className="account-form__server-error">
-                {errorInfoText}
+    <>
+      <Header loggedIn={loggedIn} />
+      <section className="profile">
+        <div className="profile__container">
+          <h2 className="profile__title">{`Привет, ${currentUser.name}!`}</h2>
+          <form
+            className="profile__form"
+            name="profile"
+            onSubmit={handleSubmit}
+          >
+            <fieldset className="profile__form-field">
+              <div className="profile__input-container">
+                <span className="profile__form-input-name">Имя</span>
+                <input
+                  className="profile__form-input"
+                  type="text"
+                  id="profile-name-input"
+                  name="profileName"
+                  value={name || ""}
+                  minLength="2"
+                  maxLength="30"
+                  required={true}
+                  noValidate
+                  autoComplete="off"
+                  disabled={isClicked ? false : errorStatus ? false : true}
+                  onChange={handleNameChange}
+                />
+              </div>
+              <span
+                className={`form__input-error name-input-error ${
+                  !nameValidation.isNameValid ? "form__input-error_active" : ""
+                }`}
+              >
+                {nameValidation.nameValidationMessage}
               </span>
-            ) : (
-              <></>
-            )}
-            {successStatus ? (
-              <span className="profile__success-request-text">
-                Профиль успешно обновлён.
+            </fieldset>
+            <fieldset className="profile__form-field">
+              <div className="profile__input-container">
+                <span className="profile__form-input-name">E-mail</span>
+                <input
+                  className="profile__form-input"
+                  id="profile-email-input"
+                  type="email"
+                  name="profileEmail"
+                  value={email || ""}
+                  autoComplete="off"
+                  required={true}
+                  noValidate
+                  disabled={isClicked ? false : errorStatus ? false : true}
+                  onChange={handleEmailChange}
+                />
+              </div>
+              <span
+                className={`form__input-error profile-email-input-error ${
+                  !emailValidation.isEmailValid
+                    ? "form__input-error_active"
+                    : ""
+                }`}
+              >
+                {emailValidation.emailValidationMessage}
               </span>
-            ) : (
-              <></>
-            )}
-            <button
-              className="profile__edit-btn"
-              type={!isClicked ? "submit" : errorStatus ? "submit" : "button"}
-              onClick={handleEditBtnClick}
-              disabled={
-                name === currentUser.name &&
-                email === currentUser.email &&
-                isClicked
-                  ? true
-                  : false
-              }
-            >
-              {isClicked
-                ? "Сохранить"
-                : errorStatus
-                ? "Сохранить"
-                : "Редактировать"}
-            </button>
-            <Link
-              className="profile__exit-btn"
-              to="/signin"
-              onClick={onSignOut}
-            >
-              Выйти из аккаунта
-            </Link>
-          </div>
-        </form>
-      </div>
-    </section>
+            </fieldset>
+            <div className="profile__footer">
+              {errorStatus ? (
+                <span className="account-form__server-error">
+                  {errorInfoText}
+                </span>
+              ) : (
+                <></>
+              )}
+              {successStatus ? (
+                <span className="profile__success-request-text">
+                  Профиль успешно обновлён.
+                </span>
+              ) : (
+                <></>
+              )}
+              <button
+                className="profile__edit-btn"
+                type={!isClicked ? "submit" : errorStatus ? "submit" : "button"}
+                onClick={handleEditBtnClick}
+                disabled={
+                  name === currentUser.name &&
+                  email === currentUser.email &&
+                  isClicked
+                    ? true
+                    : false
+                }
+              >
+                {isClicked
+                  ? "Сохранить"
+                  : errorStatus
+                  ? "Сохранить"
+                  : "Редактировать"}
+              </button>
+              <Link
+                className="profile__exit-btn"
+                to="/signin"
+                onClick={onSignOut}
+              >
+                Выйти из аккаунта
+              </Link>
+            </div>
+          </form>
+        </div>
+      </section>
+    </>
   );
 }
 
